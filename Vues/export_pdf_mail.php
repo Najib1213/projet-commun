aa92c683f8301bafddd1fb6cebe65e9b303ed63b
ArFeDias <arfd62378@eleve.isep.fr>
2025-06-17 19:54:31 +0200

    envoie mail

--- Vues/export_pdf_mail.php
+++ Vues/export_pdf_mail.php
@@ -4,10 +4,10 @@ require_once '../config/connexion_bdd.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require '../vendor/phpmailer/src/PHPMailer.php';
-require '../vendor/phpmailer/src/SMTP.php';
-require '../vendor/phpmailer/src/Exception.php';
-require '../vendor/fpdf/fpdf.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+require 'phpmailer/src/Exception.php';
+
 
 if (!isset($_SESSION['user']['email']) || empty($_POST['destinataire'])) {
     die("Erreur d'accès.");
